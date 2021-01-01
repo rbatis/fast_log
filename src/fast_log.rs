38a7959d6ebac4cd7870d881f5de4400d63999d3
zhuxiujia <zhuxiujia@qq.com>
2021-01-01 23:17:19 +0800

    recode split code

--- src/fast_log.rs
+++ src/fast_log.rs
@@ -303,7 +303,7 @@ mod test {
 
     #[test]
     pub fn test_file_compation_zip_stable_test() {
-        init_split_log("target/logs/", 1000, LogSize::MB(10), true, log::Level::Info, None, true);
+        init_log("requests.log", 1000, log::Level::Debug, None, true);
         let now = std::time::Instant::now();
         loop {
             info!("Commencing yak shaving");
