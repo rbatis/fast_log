71a23a011e50b18f5c4639f16b0c0ab2c476a330
zhuxiujia <zhuxiujia@qq.com>
2021-09-24 11:21:13 +0800

    add example

---
+++ example/src/split_log_lz4.rs
@@ -0,0 +1,24 @@
+use fast_log::consts::LogSize;
+use fast_log::plugin::file_split::RollingType;
+use fast_log::plugin::packer::LZ4Packer;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main(){
+    fast_log::init_split_log(
+        "target/logs/",
+        1000,
+        LogSize::KB(50),
+        true,
+        RollingType::KeepNum(5),
+        log::Level::Info,
+        None,
+        Box::new(LZ4Packer{}),
+        true,
+    );
+    for _ in 0..20000 {
+        log::info!("Commencing yak shaving");
+    }
+    sleep(Duration::from_secs(1));
+    println!("you can see log files in path: {}","target/logs/")
+}
