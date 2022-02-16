7bf177888a81af93f3a9d5c84482a27c1999f413
zhuxiujie <zhuxiujia@qq.com>
2022-02-16 15:51:21 +0800

    use mco replace cogo

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,75 +1,75 @@
 
 use std::time::Duration;
 
-/// if use cogo runtime
-#[cfg(feature = "cogo")]
-pub type TcpListener = cogo::net::TcpListener;
-#[cfg(feature = "cogo")]
-pub type TcpStream = cogo::net::TcpStream;
-#[cfg(feature = "cogo")]
-pub type Receiver<T> = cogo::std::sync::channel::Receiver<T>;
-#[cfg(feature = "cogo")]
-pub type Sender<T> = cogo::std::sync::channel::Sender<T>;
-#[cfg(feature = "cogo")]
-pub type JoinHandle<T> = cogo::coroutine::JoinHandle<T>;
-#[cfg(feature = "cogo")]
-pub type Mutex<T> = cogo::std::sync::Mutex<T>;
-#[cfg(feature = "cogo")]
-pub type WaitGroup = cogo::std::sync::WaitGroup;
+/// if use mco runtime
+#[cfg(feature = "mco")]
+pub type TcpListener = mco::net::TcpListener;
+#[cfg(feature = "mco")]
+pub type TcpStream = mco::net::TcpStream;
+#[cfg(feature = "mco")]
+pub type Receiver<T> = mco::std::sync::channel::Receiver<T>;
+#[cfg(feature = "mco")]
+pub type Sender<T> = mco::std::sync::channel::Sender<T>;
+#[cfg(feature = "mco")]
+pub type JoinHandle<T> = mco::coroutine::JoinHandle<T>;
+#[cfg(feature = "mco")]
+pub type Mutex<T> = mco::std::sync::Mutex<T>;
+#[cfg(feature = "mco")]
+pub type WaitGroup = mco::std::sync::WaitGroup;
 
-#[cfg(feature = "cogo")]
+#[cfg(feature = "mco")]
 pub fn chan<T>() -> (Sender<T>, Receiver<T>) {
-    cogo::chan!()
+    mco::chan!()
 }
 
-#[cfg(feature = "cogo")]
+#[cfg(feature = "mco")]
 pub fn sleep(d: Duration) {
-    cogo::coroutine::sleep(d)
+    mco::coroutine::sleep(d)
 }
 
-#[cfg(feature = "cogo")]
+#[cfg(feature = "mco")]
 pub fn spawn<F>(f: F) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
-    cogo::coroutine::Builder::new().stack_size(0x1000).spawn(f)
+    mco::coroutine::Builder::new().stack_size(0x1000).spawn(f)
 }
 
-#[cfg(feature = "cogo")]
+#[cfg(feature = "mco")]
 pub fn spawn_stack_size<F>(f: F, stack_size:usize) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
-    cogo::coroutine::Builder::new().stack_size(stack_size).spawn(f)
+    mco::coroutine::Builder::new().stack_size(stack_size).spawn(f)
 }
 
 
-/// if not cogo
-#[cfg(not(feature = "cogo"))]
+/// if not mco
+#[cfg(not(feature = "mco"))]
 pub type TcpListener = std::net::TcpListener;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type TcpStream = std::net::TcpStream;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type Receiver<T> = crossbeam::channel::Receiver<T>;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type Sender<T> = crossbeam::channel::Sender<T>;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type JoinHandle<T> = std::thread::JoinHandle<T>;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type Mutex<T> = std::sync::Mutex<T>;
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub type WaitGroup = crossbeam_utils::sync::WaitGroup;
 
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub fn chan<T>() -> (Sender<T>, Receiver<T>) {
     crossbeam::channel::unbounded()
 }
 
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub fn sleep(d: Duration) {
     std::thread::sleep(d)
 }
 
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub fn spawn<F>(f: F) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
     std::thread::spawn(f)
 }
 
-#[cfg(not(feature = "cogo"))]
+#[cfg(not(feature = "mco"))]
 pub fn spawn_stack_size<F>(f: F, stack_size:usize) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
     std::thread::spawn(f)
 }
