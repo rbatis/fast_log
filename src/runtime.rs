ce7f63e2a37a5efd1ab73e283e060fbbafceb59b
zxj <zhuxiujia@qq.com>
2022-03-07 11:29:35 +0800

    reset to runtime thread

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,42 +1,4 @@
-
 use std::time::Duration;
-
-/// if use mco runtime
-#[cfg(feature = "mco")]
-pub type TcpListener = mco::net::TcpListener;
-#[cfg(feature = "mco")]
-pub type TcpStream = mco::net::TcpStream;
-#[cfg(feature = "mco")]
-pub type Receiver<T> = mco::std::sync::channel::Receiver<T>;
-#[cfg(feature = "mco")]
-pub type Sender<T> = mco::std::sync::channel::Sender<T>;
-#[cfg(feature = "mco")]
-pub type JoinHandle<T> = mco::coroutine::JoinHandle<T>;
-#[cfg(feature = "mco")]
-pub type Mutex<T> = mco::std::sync::Mutex<T>;
-#[cfg(feature = "mco")]
-pub type WaitGroup = mco::std::sync::WaitGroup;
-
-#[cfg(feature = "mco")]
-pub fn chan<T>() -> (Sender<T>, Receiver<T>) {
-    mco::chan!()
-}
-
-#[cfg(feature = "mco")]
-pub fn sleep(d: Duration) {
-    mco::coroutine::sleep(d)
-}
-
-#[cfg(feature = "mco")]
-pub fn spawn<F>(f: F) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
-    mco::coroutine::Builder::new().stack_size(0x1000).spawn(f)
-}
-
-#[cfg(feature = "mco")]
-pub fn spawn_stack_size<F>(f: F, stack_size:usize) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
-    mco::coroutine::Builder::new().stack_size(stack_size).spawn(f)
-}
-
 
 /// if not mco
 #[cfg(not(feature = "mco"))]
