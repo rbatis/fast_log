18950ccebea2b42c55301c6d20c43451d175c045
zxj <zhuxiujia@qq.com>
2022-03-18 14:48:19 +0800

    add tokio example

--- example/src/custom_appender_tokio.rs
+++ example/src/custom_appender_tokio.rs
@@ -43,7 +43,7 @@ impl LogAppender for CustomLog {
 #[tokio::main]
 async fn main() {
     let wait = fast_log::init(Config::new().custom(CustomLog {
-        rt: tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap()
+        rt: tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap()
     })).unwrap();
     log::info!("Commencing yak shaving");
     log::error!("Commencing error");
