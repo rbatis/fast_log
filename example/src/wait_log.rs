c18013549ff1fdcf161908387373bd6d965411e0
zxj <zhuxiujia@qq.com>
2022-03-18 14:47:39 +0800

    add tokio example

--- example/src/wait_log.rs
+++ example/src/wait_log.rs
@@ -1,9 +1,11 @@
+use log::Level;
 use fast_log::config::Config;
 
 fn main(){
     let wait=fast_log::init(Config::new()
+        .level(Level::Debug)
         .console()
-        .file("requests.log")).unwrap();
+        .file("target/requests.log")).unwrap();
     log::debug!("Commencing yak shaving{}", 0);
     wait.wait();
 }
