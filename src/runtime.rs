a9314a44108638586b761f4b4481558a4eccbb83
zxj <zhuxiujia@qq.com>
2022-05-18 16:44:09 +0800

    change feature

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,37 +1,37 @@
 use std::time::Duration;
 
 /// if not mco
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type TcpListener = std::net::TcpListener;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type TcpStream = std::net::TcpStream;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type Receiver<T> = crossbeam::channel::Receiver<T>;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type Sender<T> = crossbeam::channel::Sender<T>;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type JoinHandle<T> = std::thread::JoinHandle<T>;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type Mutex<T> = std::sync::Mutex<T>;
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub type WaitGroup = crossbeam_utils::sync::WaitGroup;
 
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub fn chan<T>() -> (Sender<T>, Receiver<T>) {
     crossbeam::channel::unbounded()
 }
 
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub fn sleep(d: Duration) {
     std::thread::sleep(d)
 }
 
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub fn spawn<F>(f: F) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
     std::thread::spawn(f)
 }
 
-#[cfg(not(feature = "mco"))]
+#[cfg(feature = "runtime_thread")]
 pub fn spawn_stack_size<F>(f: F, stack_size:usize) -> JoinHandle<()> where F: FnOnce() + std::marker::Send + 'static {
     std::thread::spawn(f)
 }
