cae49ef3fa695ab70d3a33d6f10ffac5027ed1fb
zxj <zhuxiujia@qq.com>
2022-05-18 14:18:35 +0800

    edit bench

--- example/src/bench_test.rs
+++ example/src/bench_test.rs
@@ -3,23 +3,22 @@ use std::time::{Instant};
 use fast_log::bencher::QPS;
 use fast_log::config::Config;
 
-struct BenchRecvLog {}
-
-impl LogAppender for BenchRecvLog {
-    fn do_log(&self, _: &FastLogRecord) {
-        //do nothing
-    }
-}
-
 /// cargo run --release --package example --bin bench_test
 fn main() {
+    struct BenchRecvLog {}
+    impl LogAppender for BenchRecvLog {
+        fn do_log(&self, _: &FastLogRecord) {
+            //do nothing
+        }
+    }
     fast_log::init(Config::new().custom(BenchRecvLog{}));
     let total = 1000000;
     let now = Instant::now();
     for index in 0..total {
         log::info!("Commencing yak shaving{}", index);
     }
+    //wait log finish write all
+    log::logger().flush();
     now.time(total);
     now.qps(total);
-    log::logger().flush();
 }
