1f432fa03833d91714c328c6f556282750fbb803
zxj <zhuxiujia@qq.com>
2022-05-27 13:45:12 +0800

    clear code

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,18 +1,11 @@
 use std::time::Duration;
 
-/// if not mco
-#[cfg(feature = "runtime_thread")]
-pub type TcpListener = std::net::TcpListener;
-#[cfg(feature = "runtime_thread")]
-pub type TcpStream = std::net::TcpStream;
 #[cfg(feature = "runtime_thread")]
 pub type Receiver<T> = crossbeam::channel::Receiver<T>;
 #[cfg(feature = "runtime_thread")]
 pub type Sender<T> = crossbeam::channel::Sender<T>;
 #[cfg(feature = "runtime_thread")]
 pub type JoinHandle<T> = std::thread::JoinHandle<T>;
-#[cfg(feature = "runtime_thread")]
-pub type Mutex<T> = std::sync::Mutex<T>;
 #[cfg(feature = "runtime_thread")]
 pub type WaitGroup = crossbeam_utils::sync::WaitGroup;
 
