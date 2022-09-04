cfcd4a7f31599b0038e2e14543b4cf48315bd25b
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-09-04 21:01:32 +0800

    add test

---
+++ benches/log.rs
@@ -0,0 +1,22 @@
+#![feature(test)]
+#![feature(bench_black_box)]
+extern crate test;
+
+use test::Bencher;
+use fast_log::appender::{FastLogRecord, LogAppender};
+use fast_log::Config;
+
+
+#[bench]
+fn bench_log(b: &mut Bencher) {
+    struct BenchRecvLog {}
+    impl LogAppender for BenchRecvLog {
+        fn do_logs(&self, _records: &[FastLogRecord]) {
+            //nothing
+        }
+    }
+    fast_log::init(Config::new().custom(BenchRecvLog {})).unwrap();
+    b.iter(|| {
+        log::info!("Commencing yak shaving");
+    });
+}
