338293c4e52d2efe7baf3fc6a0fc7c66d59ad228
zxj <zhuxiujia@qq.com>
2022-10-28 14:41:51 +0800

    fix #16

--- src/plugin/file_split.rs
+++ src/plugin/file_split.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::fmt::{Display, Formatter};
 use std::fs::{DirEntry, File, OpenOptions};
 use std::io::{Seek, SeekFrom, Write};
 
@@ -6,6 +7,7 @@ use crate::appender::{Command, FastLogRecord, LogAppender};
 use crate::consts::LogSize;
 use crate::error::LogError;
 use crate::{chan, Receiver, Sender};
+use fastdate::{DateTime, Time};
 use std::str::FromStr;
 use std::time::Duration;
 
@@ -131,6 +133,24 @@ pub struct FileSplitAppenderData {
     temp_name: String,
 }
 
+pub struct DateTimeFile(DateTime);
+impl Display for DateTimeFile {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let mut buf: [u8; 29] = *b"0000-00-00_00:00:00.000000000";
+        buf[0] = b'0' + (self.0.year / 1000) as u8;
+        buf[1] = b'0' + (self.0.year / 100 % 10) as u8;
+        buf[2] = b'0' + (self.0.year / 10 % 10) as u8;
+        buf[3] = b'0' + (self.0.year % 10) as u8;
+        buf[5] = b'0' + (self.0.mon / 10) as u8;
+        buf[6] = b'0' + (self.0.mon % 10) as u8;
+        buf[8] = b'0' + (self.0.day / 10) as u8;
+        buf[9] = b'0' + (self.0.day % 10) as u8;
+        let time = Time::from(self.0.clone());
+        let len = time.display_time(11, &mut buf);
+        f.write_str(std::str::from_utf8(&buf[..len]).unwrap())
+    }
+}
+
 impl FileSplitAppenderData {
     /// send data make an pack,and truncate data when finish.
     pub fn send_pack(&mut self) {
@@ -139,7 +159,7 @@ impl FileSplitAppenderData {
             "{}{}{}.log",
             self.dir_path,
             &self.temp_name,
-            format!("{:29}", fastdate::DateTime::now())
+            DateTimeFile(fastdate::DateTime::now())
         );
         std::fs::copy(&first_file_path, &new_log_name);
         self.sender.send(LogPack {
