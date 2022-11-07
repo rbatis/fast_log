6b1050016f232631f310a7d5b22b780ce51aed30
zxj <zhuxiujia@qq.com>
2022-11-07 16:56:04 +0800

    remove dead code

--- example/src/bench_test_file_split.rs
+++ example/src/bench_test_file_split.rs
@@ -1,6 +1,6 @@
 use fast_log::bencher::TPS;
 use fast_log::config::Config;
-use fast_log::consts::{LogSize, SplitType};
+use fast_log::consts::LogSize;
 use fast_log::plugin::file_split::RollingType;
 use fast_log::plugin::packer::LogPacker;
 use std::time::Instant;
@@ -11,7 +11,7 @@ fn main() {
     let _ = std::fs::remove_dir("target/logs/");
     fast_log::init(Config::new().file_split(
         "target/logs/",
-        SplitType::Size(LogSize::MB(1)),
+        LogSize::MB(1),
         RollingType::All,
         LogPacker {},
     ))
--- example/src/split_log_flush.rs
+++ example/src/split_log_flush.rs
@@ -1,12 +1,12 @@
 use fast_log::config::Config;
-use fast_log::consts::{LogSize, SplitType};
+use fast_log::consts::LogSize;
 use fast_log::plugin::file_split::RollingType;
 use fast_log::plugin::packer::LogPacker;
 
 fn main() {
     fast_log::init(Config::new().console().file_split(
         "target/logs/",
-        SplitType::Size(LogSize::MB(1)),
+        LogSize::MB(1),
         RollingType::All,
         LogPacker {},
     ))
