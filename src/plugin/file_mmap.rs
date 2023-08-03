545b06e1dd5bcacc4f02ebdc7b54275c547a0f25
zxj <zhuxiujia@qq.com>
2023-08-03 13:12:32 +0800

    add SplitFile

--- src/plugin/file_mmap.rs
+++ src/plugin/file_mmap.rs
@@ -1,4 +1,5 @@
 use crate::appender::{FastLogRecord, LogAppender};
+use crate::consts::LogSize;
 use crate::error::LogError;
 use crate::plugin::file_split::SplitFile;
 use memmap::MmapMut;
@@ -6,13 +7,15 @@ use std::cell::{RefCell, UnsafeCell};
 use std::fs::{File, Metadata, OpenOptions};
 use std::io::SeekFrom;
 
+/// file size must = temp_size
 pub struct MmapFile {
     file: RefCell<File>,
     bytes: UnsafeCell<MmapMut>,
+    size: LogSize,
 }
 
 impl MmapFile {
-    pub fn new(log_file_path: &str, size: usize) -> Result<Self, LogError> {
+    pub fn new(log_file_path: &str, size: LogSize) -> Result<Self, LogError> {
         let log_file_path = log_file_path.replace("\\", "/");
         if let Some(right) = log_file_path.rfind("/") {
             let path = &log_file_path[0..right];
@@ -22,7 +25,7 @@ impl MmapFile {
             .create(true)
             .append(true)
             .open(&log_file_path)?;
-        file.set_len(size as u64);
+        file.set_len(size.get_len() as u64);
         let mmap = unsafe {
             memmap::MmapOptions::new()
                 .map(&file)
@@ -31,6 +34,7 @@ impl MmapFile {
         Ok(Self {
             file: RefCell::new(file),
             bytes: UnsafeCell::new(mmap.make_mut()?),
+            size,
         })
     }
 }
