9f1b9199fd97d3ec9815e57fe432ee0f05d052a2
zxj <zhuxiujia@qq.com>
2023-08-03 13:47:45 +0800

    add SplitFile

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,7 @@
+use crate::error::LogError;
+
 pub enum LogSize {
+    B(usize),
     KB(usize),
     MB(usize),
     GB(usize),
@@ -9,11 +12,42 @@ pub enum LogSize {
 impl LogSize {
     pub fn get_len(&self) -> usize {
         match self {
+            Self::B(b) => *b,
             Self::KB(kb) => kb * 1024,
             Self::MB(mb) => mb * 1024 * 1024,
             Self::GB(gb) => gb * 1024 * 1024 * 1024,
             Self::TB(tb) => tb * 1024 * 1024 * 1024 * 1024,
             Self::EB(eb) => eb * 1024 * 1024 * 1024 * 1024 * 1024,
+        }
+    }
+
+    pub fn parse(value: &str) -> Result<Self, LogError> {
+        if value.ends_with("EB") {
+            Ok(Self::EB(
+                value.trim_end_matches("EB").parse().unwrap_or_default(),
+            ))
+        } else if value.ends_with("TB") {
+            Ok(Self::TB(
+                value.trim_end_matches("TB").parse().unwrap_or_default(),
+            ))
+        } else if value.ends_with("GB") {
+            Ok(Self::GB(
+                value.trim_end_matches("GB").parse().unwrap_or_default(),
+            ))
+        } else if value.ends_with("MB") {
+            Ok(Self::MB(
+                value.trim_end_matches("MB").parse().unwrap_or_default(),
+            ))
+        } else if value.ends_with("KB") {
+            Ok(Self::KB(
+                value.trim_end_matches("KB").parse().unwrap_or_default(),
+            ))
+        } else if value.ends_with("B") {
+            Ok(Self::B(
+                value.trim_end_matches("B").parse().unwrap_or_default(),
+            ))
+        } else {
+            Err(LogError::from("unknow LogSize"))
         }
     }
 }
--- src/plugin/file_mmap.rs
+++ src/plugin/file_mmap.rs
@@ -1,17 +1,18 @@
-use crate::appender::{FastLogRecord, LogAppender};
 use crate::consts::LogSize;
 use crate::error::LogError;
 use crate::plugin::file_split::SplitFile;
 use memmap::MmapMut;
-use std::cell::{RefCell, UnsafeCell};
+use std::cell::RefCell;
 use std::fs::{File, Metadata, OpenOptions};
 use std::io::SeekFrom;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 /// file size must = temp_size
 pub struct MmapFile {
     file: RefCell<File>,
-    bytes: UnsafeCell<MmapMut>,
+    bytes: RefCell<MmapMut>,
     size: LogSize,
+    point: AtomicU64,
 }
 
 impl MmapFile {
@@ -33,8 +34,9 @@ impl MmapFile {
         };
         Ok(Self {
             file: RefCell::new(file),
-            bytes: UnsafeCell::new(mmap.make_mut()?),
+            bytes: RefCell::new(mmap.make_mut()?),
             size,
+            point: AtomicU64::new(0),
         })
     }
 }
@@ -44,22 +46,77 @@ impl SplitFile for MmapFile {
     where
         Self: Sized,
     {
-        todo!()
+        let mut path = path.to_string();
+        if !path.contains("?") {
+            path.push_str("?1GB");
+        }
+        let index = path.rfind("?").unwrap_or_default();
+        let file_path = &path[0..index];
+        let file_size = &path[index..path.len()];
+        let size = LogSize::parse(file_size)?;
+        Ok(MmapFile::new(file_path, size)?)
     }
 
     fn seek(&self, pos: SeekFrom) -> std::io::Result<u64> {
-        todo!()
+        let len = self.size.len() as u64;
+        let new_pos = match pos {
+            SeekFrom::Start(n) => {
+                if n > len {
+                    len
+                } else {
+                    n
+                }
+            }
+            SeekFrom::End(n) => {
+                if n < 0 {
+                    let end_offset = len.saturating_add(-n as u64);
+                    if end_offset == 0 {
+                        0
+                    } else {
+                        end_offset - 1
+                    }
+                } else {
+                    len.checked_sub(n as u64)?
+                }
+            }
+            SeekFrom::Current(n) => {
+                let current = self.point.load(Ordering::Relaxed);
+                let offset = current.checked_add(n as u64)?;
+                if offset > len {
+                    len
+                } else {
+                    offset
+                }
+            }
+        };
+        self.point.store(new_pos, Ordering::SeqCst);
+        Ok(new_pos)
     }
 
     fn write(&self, buf: &[u8]) -> std::io::Result<usize> {
-        todo!()
+        let len = buf.len() as u64;
+        let size = self.size.get_len();
+        if self.point.load(Ordering::Relaxed) + len > size as u64 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Write past end of file",
+            ));
+        }
+        let mut bytes = self.bytes.borrow_mut();
+        let current = self.point.load(Ordering::Relaxed);
+        bytes.write_at(current as usize, buf)?;
+        let len = buf.len();
+        self.point.fetch_add(len as u64, Ordering::SeqCst);
+        Ok(len)
     }
 
     fn set_len(&self, len: u64) -> std::io::Result<()> {
-        todo!()
+        self.file.borrow_mut().set_len(len)?;
+        self.size.set_len(len);
+        Ok(())
     }
 
     fn metadata(&self) -> std::io::Result<Metadata> {
-        todo!()
+        self.file.borrow().metadata()
     }
 }
