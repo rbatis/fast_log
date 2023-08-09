5254f3a6ab5eed14c27f65bc64371d8a1a934470
zxj <zhuxiujia@qq.com>
2023-08-09 10:28:10 +0800

    support mmap

---
+++ benches/log_file.rs
@@ -0,0 +1,24 @@
+#![feature(test)]
+#![feature(bench_black_box)]
+extern crate test;
+
+use fast_log::Config;
+
+use test::{black_box, Bencher};
+
+// 85 ns/iter (+/- 2073)
+#[bench]
+fn bench_log_file(b: &mut Bencher) {
+    let _ = std::fs::remove_file("target/test_bench.log");
+    fast_log::init(
+        Config::new()
+            .file("target/test_bench.log")
+            .chan_len(Some(1000000)),
+    )
+    .unwrap();
+    b.iter(|| {
+        black_box({
+            log::info!("Commencing yak shaving");
+        });
+    });
+}
