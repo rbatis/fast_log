0cdb688980a6b22c7db4d5feaf63ba99c341b8f8
zxj <zhuxiujia@qq.com>
2024-07-27 21:42:26 +0800

    edit doc

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     fast_log::init(Config::new().console().chan_len(Some(100000))).unwrap();
     log::info!("Commencing yak shaving{}", 0);
     log::error!("Commencing yak shaving{}", 0);
+    log::logger().flush();
 }
