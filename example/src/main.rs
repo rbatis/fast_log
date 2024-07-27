3156571fc1a85fcc4cfd37b89dee8f42b84f8acb
zxj <zhuxiujia@qq.com>
2024-07-27 21:41:40 +0800

    edit doc

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,12 +1,7 @@
 use fast_log::config::Config;
-use fast_log::print;
 
 fn main() {
     fast_log::init(Config::new().console().chan_len(Some(100000))).unwrap();
-    for _ in 0..100 {
-        log::info!("Commencing yak shaving{}", 0);
-    }
-    print("Commencing print\n".into()).expect("fast log not init");
+    log::info!("Commencing yak shaving{}", 0);
     log::error!("Commencing yak shaving{}", 0);
-    log::logger().flush();
 }
