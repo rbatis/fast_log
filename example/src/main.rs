e76750bb11c0e87fd6195f9dcd727f2fd8da073a
zxj <zhuxiujia@qq.com>
2024-07-30 02:21:30 +0800

    add impl PackType

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,11 +1,12 @@
 use fast_log::config::Config;
+use fast_log::print;
 
 fn main() {
     fast_log::init(Config::new().console().chan_len(Some(100000))).unwrap();
     for _ in 0..100 {
         log::info!("Commencing yak shaving{}", 0);
     }
-    fast_log::print("Commencing print\n".into()).expect("fast log not init");
+    print("Commencing print\n".into()).expect("fast log not init");
     log::error!("Commencing yak shaving{}", 0);
     log::logger().flush();
 }
